955b396b285207da3526972a654da11227dd905a
Daria Zemlyanaya <zemlyanayadasha78@gmail.com>
2022-04-17 20:50:35 +0300

    set project | fourth step

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -5,6 +5,15 @@ import picocli.CommandLine;
 @CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 public class App {
+    @CommandLine.Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "";
+
+    @CommandLine.Parameters(description = "path to first file")
+    private String filepath1;
+
+    @CommandLine.Parameters(description = "path to second file")
+    private String filepath2;
+
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
