053101b35742c72e40c02190af782261ee9b3e0a
Daria Zemlyanaya <zemlyanayadasha78@gmail.com>
2022-05-08 16:51:18 +0300

    fix formatter

--- src/main/java/hexlet/code/formatters/Stylish.java
+++ src/main/java/hexlet/code/formatters/Stylish.java
@@ -10,14 +10,14 @@ public class Stylish {
         result.append("{");
         for (Map<String, Object> map : list) {
             if (map.get("status").equals("unchanged")) {
-                result.append("\n\s\s\s").append(map.get("key")).append(": ").append(map.get("oldValue"));
+                result.append("\n\s\s\s\s").append(map.get("key")).append(": ").append(map.get("oldValue"));
             } else if (map.get("status").equals("changed")) {
-                result.append("\n\s- ").append(map.get("key")).append(": ").append(map.get("oldValue"));
-                result.append("\n\s+ ").append(map.get("key")).append(": ").append(map.get("newValue"));
+                result.append("\n\s\s- ").append(map.get("key")).append(": ").append(map.get("oldValue"));
+                result.append("\n\s\s+ ").append(map.get("key")).append(": ").append(map.get("newValue"));
             } else if (map.get("status").equals("deleted")) {
-                result.append("\n\s- ").append(map.get("key")).append(": ").append(map.get("oldValue"));
+                result.append("\n\s\s- ").append(map.get("key")).append(": ").append(map.get("oldValue"));
             } else if (map.get("status").equals("added")) {
-                result.append("\n\s+ ").append(map.get("key")).append(": ").append(map.get("newValue"));
+                result.append("\n\s\s+ ").append(map.get("key")).append(": ").append(map.get("newValue"));
             }
         }
         result.append("\n").append("}");
